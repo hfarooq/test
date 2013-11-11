94d11435258478270f32ff46a4453ccbca4e6986
Hamza F <hamza93f@gmail.com>
2013-11-11 12:53:41 -0500

    Just testing out the commit

---
+++ test/test.cpp
@@ -0,0 +1,20 @@
+/*
+ * test.cpp
+ *
+ *  Created on: Nov 11, 2013
+ *      Author: Hamza
+ */
+
+
+
+
+#include <iostream>
+
+using namespace std;
+
+int main(){
+
+	cout << "This code works!\n";
+
+	return 0;
+}
